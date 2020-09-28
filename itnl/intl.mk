5947e5f131ebbb749ecc69ed114c3c4c50e15a49
Martinvlba <kailing2@live.com>
2020-09-28 16:14:06 +0300

    Fix some things

--- itnl/intl.mk
+++
@@ -1,7 +0,0 @@
-# https://github.com/gcc-mirror/gcc
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/libraries/intl
-
-PRODUCT_COPY_FILES += \
-    vendor/libraries/intl/libintl.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libintl.so   
